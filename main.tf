d2daaede17df672fe5bf6b949c413f61a70c6763
nagababudba0999 <67545689+nagababudba0999@users.noreply.github.com>
2024-01-24 09:53:46 +0530

    Update main.tf
    added sg

--- main.tf
+++ main.tf
@@ -4,12 +4,23 @@ provider "aws" {
 
 module "network" {
   source = "git::https://github.com/nagababudba0999/terraformclassrepo.git//Network"
-  vpc_cidr = "20.0.0.0/16"
+  vpc_cidr = "30.0.0.0/16"
   subnet_cidr = "20.0.1.0/24"
   vpc_tags = {
      environment = "stage"
      Project = "abc"
      owner = "nagababu"
-     Name = "stage-vpc"
+     Name = "prod-vpc"
+  }
+}
+
+module "sg" {
+  source = "git::https://github.com/nagababudba0999/terraformclassrepo.git//Security_Group"
+  sg_ports = [8001,8002,8003]
+  vpc_id = module.network.vpc_id
+  sg_cidr_block = [module.network.vpc_cidr]
+  sg_tags = {
+    Name = "prod-sg"
+    Environment = "prod"
   }
 }
